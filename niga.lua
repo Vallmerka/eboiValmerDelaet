d7d98a04e18921d00515e375b8f7ce1b7029edca
Vallmerka <Valmerka@yandex.ru>
2025-01-03 18:56:03 +0300

    Update niga.lua

--- niga.lua
+++ niga.lua
@@ -658,8 +658,8 @@ do
             Description = "Выполнение полезного скрипта.",
             Callback = function()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-                        })
-                    end  
+        })
+  
 
     else
         ShowWarning = true
